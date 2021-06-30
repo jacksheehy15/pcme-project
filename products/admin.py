2d9cef695ef42af0678a633f14f5eab9e266140a
jacksheehy15 <sheehyboi@gmail.com>
2021-06-30 16:09:01 +0000

    Added Products and search to views.py

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,8 @@
-
 from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
@@ -16,11 +16,13 @@ class ProductAdmin(admin.ModelAdmin):
 
     ordering = ('sku',)
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
