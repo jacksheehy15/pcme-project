361f07a4350dbe1fcbfda80cdb8b0c09a8c8b361
jacksheehy15 <sheehyboi@gmail.com>
2021-07-05 12:29:53 +0000

    Added shopping cart context and delivry logic

---
+++ cart/contexts.py
@@ -0,0 +1,25 @@
+from decimal import Decimal
+from django.conf import settings
+
+def cart_contents(request):
+
+    cart_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+    else:
+        delivery = 0
+  
+    grand_total = delivery + total
+      
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'grand_total': grand_total,
+    }
+
+    return context
--- pcme/settings.py
+++ pcme/settings.py
@@ -66,9 +66,10 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  # required by allauth
+                'django.template.context_processors.request',   # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents'
             ],
         },
     },
@@ -149,3 +150,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 600
+STANDARD_DELIVERY_PERCENTAGE = 1
