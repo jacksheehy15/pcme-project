84d79ab144f13dbe1a552521ae60d0d9b33822c3
jacksheehy15 <sheehyboi@gmail.com>
2021-07-08 17:01:11 +0000

    Crispy form added to settings

--- checkout/views.py
+++ checkout/views.py
@@ -3,18 +3,17 @@ from django.contrib import messages
 
 from .forms import OrderForm
 
-# Create your views here.
 
 def checkout(request):
-    cart = request.session.get('bag', {})
+    cart = request.session.get('cart', {})
     if not cart:
-        messages.error(request, "Your cart is empty at the moment!")
-        return redirect(reverse, ('products'))
-    
+        messages.error(request, "You have no products in your cart")
+        return redirect(reverse('products'))
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
     }
 
     return render(request, template, context)
