0894a8ae770d54ce09d8c6cb64cd12c7e5a50b41
jacksheehy15 <sheehyboi@gmail.com>
2021-07-08 14:24:10 +0000

    Added Toast functionlity

--- cart/views.py
+++ cart/views.py
@@ -22,7 +22,7 @@ def add_to_cart(request, item_id):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your cart')
+        messages.info(request, f'Added: {product.name} to your cart')
 
     request.session['cart'] = cart
     print(request.session['cart'])
