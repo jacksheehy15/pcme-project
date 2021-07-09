2c7879630a131a5497a365ec3d9618f6d8a5524b
jacksheehy15 <sheehyboi@gmail.com>
2021-07-09 10:43:33 +0000

    Added Stripe API key to views

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HWMrLIu09P0FTqkNB4NCwHxMKIy8Q7VGOcjBxnzbhO9aZqogXNobex6pkYh4uAf9mCFS9zOGbRR6CIi3dadG1B600cwIAlFDT',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
