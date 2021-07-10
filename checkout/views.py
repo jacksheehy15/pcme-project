5fc61448dbc52d7ada5830d1a141969a17ec3278
jacksheehy15 <sheehyboi@gmail.com>
2021-07-10 16:08:47 +0000

    Added checkout caching functionality

--- checkout/views.py
+++ checkout/views.py
@@ -11,20 +11,20 @@ from cart.contexts import cart_contents
 import stripe
 import json
 
-
 @require_POST
 def cache_checkout_data(request):
     try:
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            'bag': json.dumps(request.session.get('cart', {})),
-            'save_info:' request.POST.get('save_info'),
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
             'username': request.user,
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot be processed right now. Please try again later.')
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
 
 
@@ -47,32 +47,35 @@ def checkout(request):
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
-        if form.is_valid():
-            order = form.save(commit=False)
-            order.user = request.user
-            pid = request.POST.get("client_secret").split("_secret")[0]
-            order.stripe_pid = pid
-            order.original_cart = json.dumps(cart)
-            order.save()
-            for item_id, quantity in cart.items():
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in cart.items():
                 try:
                     product = Product.objects.get(id=item_id)
-                    order_line_item = OrderLineItem(
-                        order=order,
-                        product=product,
-                        quantity=quantity,
-                    )
-                    order_line_item.save()
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
                 except Product.DoesNotExist:
-                    messages.error(
-                        request,
-                        (
-                            "One of the products in your bag wasn't found in our database.\n\
-                                Please call us for assistance!"
-                        ),
+                    messages.error(request, (
+                        "One of the products in your cart wasn't found in our database. "
+                        "Please call us for assistance!")
                     )
                     order.delete()
-                    return redirect("cart")
+                    return redirect(reverse('view_cart'))
+
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
@@ -108,23 +111,20 @@ def checkout(request):
 
     return render(request, template, context)
 
-
 def checkout_success(request, order_number):
     """
     Handle successful checkouts
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Your order has been received \
+    messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
 
     if 'cart' in request.session:
         del request.session['cart']
-
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
     }
-
     return render(request, template, context)
